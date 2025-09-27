38fae0dc7e49b2f8e8c0aa6535e77153be873eb8
rougemeilland <28302784+rougemeilland@users.noreply.github.com>
2025-09-27 23:43:04 +0900

    20250927

--- SimpleFileNameRenumberer.CUI/Program.cs
+++ SimpleFileNameRenumberer.CUI/Program.cs
@@ -9,6 +9,8 @@ namespace SimpleFileNameRenumberer.CUI
     {
         private static int Main(string[] args)
         {
+            ProcessUtility.SetupCurrentProcessPriority();
+
             if (TinyConsole.InputEncoding.CodePage != Encoding.UTF8.CodePage || TinyConsole.OutputEncoding.CodePage != Encoding.UTF8.CodePage)
             {
                 if (OperatingSystem.IsWindows())
